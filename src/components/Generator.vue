e485b277b464ae88221cf66f64470a0b14d841af
khatabwedaa <khatabwedaa@gmail.com>
2022-10-28 00:15:18 +0200

    fix reponsive

--- src/components/Generator.vue
+++ src/components/Generator.vue
@@ -89,11 +89,11 @@
                             </div>
                         </div>
 
-                        <div class="mt-4 md:flex md:items-center md:space-x-4">
-                            <input :value="classes()" disabled class="flex-1 h-12 px-4 font-medium text-gray-700 bg-white border border-gray-200 rounded-md cursor-text focus:border-indigo-500 focus:outline-none focus:ring focus:ring-indigo-600 focus:ring-opacity-20" />
+                        <div class="mt-4 sm:flex sm:items-center sm:space-x-4 lg:block lg:space-x-0 xl:flex xl:space-x-4">
+                            <input :value="classes()" disabled class="flex-1 w-full h-12 px-4 font-medium text-gray-700 bg-white border border-gray-200 rounded-md lg:w-full xl:w-auto sm:w-auto cursor-text focus:border-indigo-500 focus:outline-none focus:ring focus:ring-indigo-600 focus:ring-opacity-20" />
                             
-                            <button v-clipboard:copy="classes()" v-clipboard:success="copied" class="flex duration-300 transition-color bg-gray-900 hover:bg-gray-700 text-white items-center justify-center w-full px-4 py-2.5 space-x-3 font-semibold rounded-md focus:outline-none sm:w-auto">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
+                            <button v-clipboard:copy="classes()" v-clipboard:success="copied" class="flex lg:w-full xl:w-auto duration-300 transition-color lg:mt-4 xl:mt-0 mt-4 sm:mt-0 bg-gray-900 hover:bg-gray-700 text-white items-center justify-center w-full px-4 py-2.5 space-x-3 font-semibold rounded-md focus:outline-none sm:w-auto">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 shrink-0">
                                 <path fill-rule="evenodd" d="M17.663 3.118c.225.015.45.032.673.05C19.876 3.298 21 4.604 21 6.109v9.642a3 3 0 01-3 3V16.5c0-5.922-4.576-10.775-10.384-11.217.324-1.132 1.3-2.01 2.548-2.114.224-.019.448-.036.673-.051A3 3 0 0113.5 1.5H15a3 3 0 012.663 1.618zM12 4.5A1.5 1.5 0 0113.5 3H15a1.5 1.5 0 011.5 1.5H12z" clip-rule="evenodd" />
                                 <path d="M3 8.625c0-1.036.84-1.875 1.875-1.875h.375A3.75 3.75 0 019 10.5v1.875c0 1.036.84 1.875 1.875 1.875h1.875A3.75 3.75 0 0116.5 18v2.625c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 013 20.625v-12z" />
                                 <path d="M10.5 10.5a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963 5.23 5.23 0 00-3.434-1.279h-1.875a.375.375 0 01-.375-.375V10.5z" />
