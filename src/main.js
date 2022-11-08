9de1b3d70380adf8f83b5a555009c2ae4d7f605e
khatabwedaa <khatabwedaa@gmail.com>
2022-11-08 20:10:05 +0200

    Add google analytics & tag

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from 'vue';
 import VueClipboard from 'vue3-clipboard'
+import VueAnalytics from 'vue3-analytics'
 import App from './App.vue';
 import "./main.css";
+import "./gtm";
 
 createApp(App).use(VueClipboard, {
     autoSetContainer: true,
     appendToBody: true,
+}).use(VueAnalytics, {
+  id: 'UA-110182129-1'
 }).mount('#app');
